ab6121f60aca2c181637697b584cd807d60650a7
smpr <gcbrown1116@gmail.com>
2018-04-19 18:29:13 -0500

    Text fields setup to allow input but not styled

--- kimmelbook/src/components/Add/bookAdd.js
+++ kimmelbook/src/components/Add/bookAdd.js
@@ -1,11 +1,99 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
+import api from 'books-mock-api'
+import { FormContainer, BodyContainer, Container, Style, TextLabelStyle } from "../Styling/DefaultStyle"
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class bookAdd extends Component {
+    state = {
+        book: {
+            title: "",
+            author: "",
+            description: "",
+            img: ""
+        },
+        redirectToBookList: false
+    }
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const newBook = await api.createBook(this.state.book);
+        this.setState({redirectToBookList: true}) 
+    }
+    handleChange = (event) => {
+        const updateBook = {
+            ...this.state.book
+        }
+        updateBook[event.target.name] = event.target.value
+        this.setState({ book: updateBook })
+    }
     render() {
+        if (this.state.redirectToBookList) {
+            return <Redirect to={`/`} />
+        }
         return (
-            <div>
-                this is the book add page
-            </div>
+            <BodyContainer>
+                <Container>
+                    <FormContainer>
+                        <div>
+                            <TextField
+                                hintText="Book Title"
+                                floatingLabelText="Book Title"
+                                floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                                floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                                onChange={this.handleChange}
+                                name="title"
+                                type="text"
+
+                                value={this.state.book.title}
+                            />
+
+                        </div>
+                        <div>
+                            <TextField
+                                hintText="Author"
+                                floatingLabelText="Author"
+                                floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                                floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                                onChange={this.handleChange}
+                                name="author"
+                                type="text"
+
+                                value={this.state.book.author}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                hintText="Image URL"
+                                floatingLabelText="Image URL"
+                                floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                                floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                                onChange={this.handleChange}
+                                name="img"
+                                type="text"
+
+                                value={this.state.book.img}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                hintText="Description"
+                                floatingLabelText="Description"
+                                floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                                floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                                onChange={this.handleChange}
+                                name="description"
+                                type="text"
+
+                                value={this.state.book.description}
+                            />
+                        </div>
+                        <div>
+                            <RaisedButton onClick={this.handleSubmit} label="Submit" style={Style} />
+                        </div>
+                    </FormContainer>
+                </Container>
+            </BodyContainer>
         );
     }
 }
