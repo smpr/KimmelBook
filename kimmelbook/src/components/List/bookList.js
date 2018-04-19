ae5291cec78805623c52427918308b433d519697
smpr <gcbrown1116@gmail.com>
2018-04-19 15:00:09 -0500

    rough styling setup

--- kimmelbook/src/components/List/bookList.js
+++ kimmelbook/src/components/List/bookList.js
@@ -1,44 +1,47 @@
 import React, { Component } from 'react';
 import api from 'books-mock-api'
 import { Link } from 'react-router-dom'
-
+import { Container, FormContainer, BodyContainer, Style, TextLabelStyle } from "../Styling/DefaultStyle"
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 
 
 class bookList extends Component {
-    state={
-        books:[],
+    state = {
+        books: [],
         users: []
-        
+
     }
-   
-    async componentWillMount(){
+//insert rating calculator based on each review rating
+    async componentWillMount() {
         try {
             //const users = await api.getUsers();
             const books = await api.getBooks();
-            this.setState({ books: books})
+            this.setState({ books: books })
             console.log(books)
             //console.log(users)
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     }
 
     render() {
         return (
-            <div>
-book Titles
-{this.state.books.map((book, index)=>{
-    return(
-        <div>
-            <img src={book.image} />
-            {book.title}
-           //insert rating from avg rating on the reviews
-        </div>
-    )
-})}
-            </div>
+            <BodyContainer>
+                
+{this.state.books.map((book, index) => {
+                    return (
+                        <div>
+                            <img src={book.image} />
+                            <center>{book.title}</center>
+                        
+                        </div>
+                    )
+                })}
+                
+            </BodyContainer>
         );
     }
 }
