310123aab81432e6cfabc39e787abc3a37f4a98f
smpr <gcbrown1116@gmail.com>
2018-04-19 13:49:17 -0500

    book mapping setup not working yet

--- kimmelbook/src/components/List/bookList.js
+++ kimmelbook/src/components/List/bookList.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import api from 'books-mock-api'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+
+
 
 class bookList extends Component {
+    state={
+        books:[],
+        users: []
+    }
+    async componentWillMount(){
+        try {
+            const users = await api.getUsers();
+            const books = await api.getBooks();
+            this.setState({ books: books.data})
+            console.log(users.data)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
     render() {
         return (
             <div>
-                This is the book list page
+book Titles
+{this.state.books.map((book, index)=>{
+    return(
+        <div>
+            {book.title}
+        </div>
+    )
+})}
             </div>
         );
     }
