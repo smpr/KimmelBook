947962267cd5723cf9e653a5cd00fa870fd6ada1
smpr <gcbrown1116@gmail.com>
2018-04-20 13:49:10 -0500

    bookInfo refactored scaleabilty

--- kimmelbook/src/components/Info/bookInfo.js
+++ kimmelbook/src/components/Info/bookInfo.js
@@ -19,8 +19,8 @@ const checkOut = function () {
 }
 
 class bookInfo extends Component {
-    state= {
-        book:{
+    state = {
+        book: {
             author: this.props.author,
             image: this.props.image,
             title: this.props.title,
@@ -46,197 +46,93 @@ class bookInfo extends Component {
         event.preventDefault();
         await api.updateBook(this.state.book)
     }
-    
+
     render() {
+        const bodyRef =
+            <div>
+                <div>
+                    <b>Title:</b> {this.props.book.title}
+                </div>
+                <div>
+                    <b>Author:</b> {this.props.book.author}
+                </div>
+                <div>
+                    {this.props.book.rating}
+                </div>
+                <div>
+                    <b>Description: </b>{this.props.book.description}
+                </div>
+                <div>
+                    <b>Availabilty: {this.isCheckedOut(this.props.book.checked_out)} </b>
+                </div>
+                <ReviewBox>
+                    {this.props.book.reviews.map((review, index) => {
+                        return (
+                            <SingleReview key={index}>
+                                <b>Stars: </b>{review.rating}<br />
+                                <b>Reviewer: </b>{makeItPretty(review.user)}
+                            </SingleReview>
+                        )
+                    })}
+                </ReviewBox>
+                <div>
+                    <center><RaisedButton onClick={this.props.toggleBookSwitch} label="Back" /></center>
+                </div>
+            </div>
         return (
             <BodyContainer>
-            <MediaQuery minDeviceWidth={1224}>
-            <BodyContainer>
-                <BookInfo>
-                    <div>
-                        <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
-                    </div>
-                    <div>
-                        <b>Title:</b> {this.props.book.title}
-                    </div>
-                    <div>
-                        <b>Author:</b> {this.props.book.author}
-                    </div>
-                    <div>
-                        {this.props.book.rating}
-                    </div>
-                    <div>
-                        <b>Description: </b>{this.props.book.description}
-                    </div>
-                    <div>
-                        <b>Availabilty: {this.isCheckedOut(this.props.book.checked_out)} </b>
-                    </div>
-                    <ReviewBox>
-                        {this.props.book.reviews.map((review, index) => {
-                            return (
-                                <SingleReview key={index}>
-                                    <b>Stars: </b>{review.rating}<br />
-                                    <b>Reviewer: </b>{makeItPretty(review.user)}
-                                </SingleReview>
-                            )
-                        })}
-                    </ReviewBox>
-                    <div>
-                        <center><RaisedButton onClick={this.props.toggleBookSwitch} label="Back" /></center>
-                    </div>
-                </BookInfo>
+                <MediaQuery minDeviceWidth={1224}>
+                    <BodyContainer>
+                        <BookInfo>
+                            <div>
+                                <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
+                            </div>
+                            {bodyRef}
+                        </BookInfo>
+                    </BodyContainer>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={375}>
+                    <BodyContainer width="100">
+                        <BookInfo>
+                            <div>
+                                <center><img src={this.props.book.image} alt={this.props.book.title} width="100" /></center>
+                            </div>
+                            {bodyRef}
+                        </BookInfo>
+                    </BodyContainer>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={414} minDeviceWidth={376}>
+                    <BodyContainer>
+                        <BookInfo>
+                            <div>
+                                <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
+                            </div>
+                            {bodyRef}
+                        </BookInfo>
+                    </BodyContainer>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={768} minDeviceWidth={415}>
+                    <BodyContainer>
+                        <BookInfo>
+                            <div>
+                                <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
+                            </div>
+                            {bodyRef}
+                        </BookInfo>
+                    </BodyContainer>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={1024} minDeviceWidth={769}>
+                    <BodyContainer>
+                        <BookInfo>
+                            <div>
+                                <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
+                            </div>
+                            {bodyRef}
+                        </BookInfo>
+                    </BodyContainer>
+                </MediaQuery>
             </BodyContainer>
-            </MediaQuery>
-            <MediaQuery maxDeviceWidth={375}>
-            <BodyContainer width ="100">
-                <BookInfo>
-                    <div>
-                        <center><img src={this.props.book.image} alt={this.props.book.title} width="100" /></center>
-                    </div>
-                    <div>
-                        <b>Title:</b> {this.props.book.title}
-                    </div>
-                    <div>
-                        <b>Author:</b> {this.props.book.author}
-                    </div>
-                    <div>
-                        {this.props.book.rating}
-                    </div>
-                    <div>
-                        <b>Description: </b>{this.props.book.description}
-                    </div>
-                    <div>
-                        <b>Availabilty: {this.isCheckedOut(this.props.book.checked_out)} </b>
-                    </div>
-                    <ReviewBox>
-                        {this.props.book.reviews.map((review, index) => {
-                            return (
-                                <SingleReview key={index}>
-                                    <b>Stars: </b>{review.rating}<br />
-                                    <b>Reviewer: </b>{makeItPretty(review.user)}
-                                </SingleReview>
-                            )
-                        })}
-                    </ReviewBox>
-                    <div>
-                        <center><RaisedButton onClick={this.props.toggleBookSwitch} label="Back" /></center>
-                    </div>
-                </BookInfo>
-            </BodyContainer>
-            </MediaQuery>
-            <MediaQuery maxDeviceWidth={414} minDeviceWidth={376}>
-            <BodyContainer>
-                <BookInfo>
-                    <div>
-                        <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
-                    </div>
-                    <div>
-                        <b>Title:</b> {this.props.book.title}
-                    </div>
-                    <div>
-                        <b>Author:</b> {this.props.book.author}
-                    </div>
-                    <div>
-                        {this.props.book.rating}
-                    </div>
-                    <div>
-                        <b>Description: </b>{this.props.book.description}
-                    </div>
-                    <div>
-                        <b>Availabilty: {this.isCheckedOut(this.props.book.checked_out)} </b>
-                    </div>
-                    <ReviewBox>
-                        {this.props.book.reviews.map((review, index) => {
-                            return (
-                                <SingleReview key={index}>
-                                    <b>Stars: </b>{review.rating}<br />
-                                    <b>Reviewer: </b>{makeItPretty(review.user)}
-                                </SingleReview>
-                            )
-                        })}
-                    </ReviewBox>
-                    <div>
-                        <center><RaisedButton onClick={this.props.toggleBookSwitch} label="Back" /></center>
-                    </div>
-                </BookInfo>
-            </BodyContainer>
-            </MediaQuery>
-            <MediaQuery maxDeviceWidth={768} minDeviceWidth={415}>
-            <BodyContainer>
-                <BookInfo>
-                    <div>
-                        <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
-                    </div>
-                    <div>
-                        <b>Title:</b> {this.props.book.title}
-                    </div>
-                    <div>
-                        <b>Author:</b> {this.props.book.author}
-                    </div>
-                    <div>
-                        {this.props.book.rating}
-                    </div>
-                    <div>
-                        <b>Description: </b>{this.props.book.description}
-                    </div>
-                    <div>
-                        <b>Availabilty: {this.isCheckedOut(this.props.book.checked_out)} </b>
-                    </div>
-                    <ReviewBox>
-                        {this.props.book.reviews.map((review, index) => {
-                            return (
-                                <SingleReview key={index}>
-                                    <b>Stars: </b>{review.rating}<br />
-                                    <b>Reviewer: </b>{makeItPretty(review.user)}
-                                </SingleReview>
-                            )
-                        })}
-                    </ReviewBox>
-                    <div>
-                        <center><RaisedButton onClick={this.props.toggleBookSwitch} label="Back" /></center>
-                    </div>
-                </BookInfo>
-            </BodyContainer>
-            </MediaQuery>
-            <MediaQuery maxDeviceWidth={1024} minDeviceWidth={769}>
-            <BodyContainer>
-                <BookInfo>
-                    <div>
-                        <center><img src={this.props.book.image} alt={this.props.book.title} /></center>
-                    </div>
-                    <div>
-                        <b>Title:</b> {this.props.book.title}
-                    </div>
-                    <div>
-                        <b>Author:</b> {this.props.book.author}
-                    </div>
-                    <div>
-                        {this.props.book.rating}
-                    </div>
-                    <div>
-                        <b>Description: </b>{this.props.book.description}
-                    </div>
-                    <div>
-                        <b>Availabilty: {this.isCheckedOut(this.props.book.checked_out)} </b>
-                    </div>
-                    <ReviewBox>
-                        {this.props.book.reviews.map((review, index) => {
-                            return (
-                                <SingleReview key={index}>
-                                    <b>Stars: </b>{review.rating}<br />
-                                    <b>Reviewer: </b>{makeItPretty(review.user)}
-                                </SingleReview>
-                            )
-                        })}
-                    </ReviewBox>
-                    <div>
-                        <center><RaisedButton onClick={this.props.toggleBookSwitch} label="Back" /></center>
-                    </div>
-                </BookInfo>
-            </BodyContainer>
-            </MediaQuery>
-        </BodyContainer>
-         
+
         );
     }
 }
